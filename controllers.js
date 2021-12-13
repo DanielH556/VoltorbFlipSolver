c88708c605edc2ecde6bd1d463a1c5ea247a9f8e
Daniel Hossaki <daniel.hossaki@outlook.com>
2021-12-13 14:30:34 -0300

    Database creation and Front-End base finished

--- controllers.js
+++ controllers.js
@@ -1,9 +1,22 @@
 var app = angular.module("voltorbFlipGame", []);
 // A
 app.controller('TableCtrl', ['$scope', function($scope) {
-  $scope.flipCard = function(x) {
-    console.log(x);
-    $scope.cardId = x;
-    $scope.angular.getElementById(`card-5`).style.backgroundColor = "#d6a185";
-  };
+  $scope.flipCard = function(cardId, cardState, cardValue) {
+    var cardIcon = 'Voltorb.png'
+
+    if (cardState == 0) {
+      document.getElementById(`card-${cardId}`).style.backgroundColor = '#d6a185';
+      cardState = 1;
+    };
+
+    if (cardValue == 1) {
+      document.getElementById(`card-${cardId}`).innerHTML = `<h2>${cardValue}</h2>`;
+    }else if (cardValue == 2) {
+      document.getElementById(`card-${cardId}`).innerHTML = `<h2>${cardValue}</h2>`;
+    }else if (cardValue == 3) {
+      document.getElementById(`card-${cardId}`).innerHTML = `<h2>${cardValue}</h2>`;
+    }else if (cardValue == 0) {
+      document.getElementById(`card-${cardId}`).innerHTML = `<img src="${cardIcon}" name="card-image-${cardId}"/>`;
+    };
+  }
 }]);
--- generation.js
+++ generation.js
@@ -1,45 +1,8 @@
-cards = [
-  // Contagem de row e column feita de cima para baixo, da esquerda para a direita.
-  { id: 1, row: 1, column: 1, state: 'covered', value: 1, image: 'Voltorb.png', memoLine: [] },
-  { id: 2, row: 1, column: 2, state: 'covered', value: 1, image: 'Voltorb.png', memoLine: [] },
-  { id: 3, row: 1, column: 3, state: 'covered', value: 1, image: 'Voltorb.png', memoLine: [] },
-  { id: 4, row: 1, column: 4, state: 'covered', value: 1, image: 'Voltorb.png', memoLine: [] },
-  { id: 5, row: 1, column: 5, state: 'covered', value: 2, image: 'Voltorb.png', memoLine: [] },
-  { id: 6, row: 2, column: 1, state: 'covered', value: 2, image: 'Voltorb.png', memoLine: [] },
-  { id: 7, row: 2, column: 2, state: 'covered', value: 2, image: 'Voltorb.png', memoLine: [] },
-  { id: 8, row: 2, column: 3, state: 'covered', value: 2, image: 'Voltorb.png', memoLine: [] },
-  { id: 9, row: 2, column: 4, state: 'covered', value: 3, image: 'Voltorb.png', memoLine: [] },
-  { id: 10, row: 2, column: 5, state: 'covered', value: 3, image: 'Voltorb.png', memoLine: [] },
-  { id: 11, row: 3, column: 1, state: 'covered', value: 3, image: 'Voltorb.png', memoLine: [] },
-  { id: 12, row: 3, column: 2, state: 'covered', value: 3, image: 'Voltorb.png', memoLine: [] },
-  { id: 13, row: 3, column: 3, state: 'covered', value: 0, image: 'Voltorb.png', memoLine: [] },
-  { id: 14, row: 3, column: 4, state: 'covered', value: 0, image: 'Voltorb.png', memoLine: [] },
-  { id: 15, row: 3, column: 5, state: 'covered', value: 0, image: 'Voltorb.png', memoLine: [] },
-  { id: 16, row: 4, column: 1, state: 'covered', value: 0, image: 'Voltorb.png', memoLine: [] },
-  { id: 17, row: 4, column: 2, state: 'covered', value: 1, image: 'Voltorb.png', memoLine: [] },
-  { id: 18, row: 4, column: 3, state: 'covered', value: 2, image: 'Voltorb.png', memoLine: [] },
-  { id: 19, row: 4, column: 4, state: 'covered', value: 3, image: 'Voltorb.png', memoLine: [] },
-  { id: 20, row: 4, column: 5, state: 'covered', value: 0, image: 'Voltorb.png', memoLine: [] },
-  { id: 21, row: 5, column: 1, state: 'covered', value: 1, image: 'Voltorb.png', memoLine: [] },
-  { id: 22, row: 5, column: 3, state: 'covered', value: 2, image: 'Voltorb.png', memoLine: [] },
-  { id: 23, row: 5, column: 3, state: 'covered', value: 3, image: 'Voltorb.png', memoLine: [] },
-  { id: 24, row: 5, column: 4, state: 'covered', value: 0, image: 'Voltorb.png', memoLine: [] },
-  { id: 25, row: 5, column: 5, state: 'covered', value: 1, image: 'Voltorb.png', memoLine: [] },
-];
-
-labels = [
-  { id: 1, sum: 4, voltorb: 4 },
-  { id: 2, sum: 4, voltorb: 4 },
-  { id: 3, sum: 4, voltorb: 4 },
-  { id: 4, sum: 4, voltorb: 4 },
-  { id: 5, sum: 4, voltorb: 4 },
-];
-
 (function createBoard(card) {
   var container = document.getElementById('board');
   var elementCnt = '';
   cards.forEach(card => {
-    elementCnt += `<button class="card" type="button" id="card-${card.id}" ng-click="flipCard(${card.id})"></button>`;
+    elementCnt += `<button class="card" type="button" id="card-${card.id}" ng-click="flipCard(${card.id},${card.state},${card.value},${card.image})"></button>`;
   });
 
   container.innerHTML = elementCnt;
